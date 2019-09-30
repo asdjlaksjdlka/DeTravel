d6e7bcddb41b0e52f4848168fb3efa2cc50e9169
123456789@qq.com <1125822350@qq.com>
2019-09-30 10:30:46 +0800

    实现user序列化，还有idea远程连接数据库

--- src/main/java/com/qf/detravel/entity/User.java
+++ src/main/java/com/qf/detravel/entity/User.java
@@ -1,6 +1,8 @@
 package com.qf.detravel.entity;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
     private Integer uId;
     private String uName;
     private String uNickName;

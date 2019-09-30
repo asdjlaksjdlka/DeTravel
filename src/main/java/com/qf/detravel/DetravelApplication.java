20a6e9e609d08a6be86e68c8998e142b9e200d73
sss <980951644@qq.com>
2019-09-30 09:20:11 +0800

    Initial commit

---
+++ src/main/java/com/qf/detravel/DetravelApplication.java
@@ -0,0 +1,13 @@
+package com.qf.detravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DetravelApplication.class, args);
+    }
+
+}

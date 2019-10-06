e776a50cb77f3812465f1edcfcee435f93a465ab
123456789@qq.com <1125822350@qq.com>
2019-10-06 17:10:35 +0800

    接口文档

--- src/main/java/com/qf/detravel/controller/ScenicController.java
+++ src/main/java/com/qf/detravel/controller/ScenicController.java
@@ -5,6 +5,8 @@ import com.qf.detravel.entity.Scenic;
 import com.qf.detravel.service.ScenicService;
 import com.qf.detravel.vo.ScenicVo;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -22,25 +24,53 @@ public class ScenicController {
      */
     @Autowired
     private ScenicService scenicService;
+
+
     @ApiOperation(value="查询所有的景点信息", notes="根据分类id和地区来查询所有的景点信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "vId", value = "需要的类别id"),
+            @ApiImplicitParam(name = "region", value = "需要的地区名称")
+    })
     @GetMapping("/allScenic.do")
     public JsonResult findAllScenic(Integer vId, String region){
         List<Scenic> allScenic = scenicService.findAllScenic(vId, region);
         return new JsonResult(1, allScenic);
     }
+
+
     @ApiOperation(value="查询地区所属国家所有的景点信息", notes="根据分类id，地区和国家来查询所有的景点信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "vId", value = "需要的类别id"),
+            @ApiImplicitParam(name = "region", value = "需要的地区名称"),
+            @ApiImplicitParam(name = "country", value = "需要的国家名称")
+    })
     @GetMapping("/countryScenic.do")
     public JsonResult findAllByCountry(Integer vId, String region, String country) {
         List<Scenic> allByCountry = scenicService.findAllByCountry(vId, region, country);
         return new JsonResult(1, allByCountry);
     }
+
+
     @ApiOperation(value="查询地区所属国家和国家内景点个数", notes="根据分类id，地区和国家来查询所有的景点信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "vId", value = "需要的类别id"),
+            @ApiImplicitParam(name = "region", value = "需要的地区名称"),
+            @ApiImplicitParam(name = "country", value = "需要的国家名称")
+    })
     @GetMapping("/scenicCount.do")
     public JsonResult findCountryCount(Integer vId, String region, String country) {
         Map<String, Integer> map = scenicService.findCountryCount(vId, region, country);
         return new JsonResult(1, map);
     }
+
+
     @ApiOperation(value="查询景点详细信息", notes="根据分类id，景点名称，地区和国家查询")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "vId", value = "需要的类别id"),
+            @ApiImplicitParam(name = "name", value = "需要的景点名称"),
+            @ApiImplicitParam(name = "region", value = "需要的地区名称"),
+            @ApiImplicitParam(name = "country", value = "需要的国家名称")
+    })
     @GetMapping("/scenicDesc.do")
     public JsonResult findScenic(Integer vId, String name, String region, String country){
         ScenicVo scenicVo = scenicService.findScenic(vId, name, region, country);

2371bc76c25be590a834a7e562903b343fb9e31b
yqz <yqzzyg@163.com>
2019-10-07 13:30:48 +0800

    测试注册

--- src/main/java/com/qf/detravel/filter/CrossFilter.java
+++ src/main/java/com/qf/detravel/filter/CrossFilter.java
@@ -15,17 +15,7 @@ public class CrossFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-        //任意ip
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        //response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization,accept,content-type,token,uname");
-        //请求头
-        response.setHeader("Access-Control-Allow-Headers", "*");
-        //请求方式
-        response.setHeader("Access-Control-Allow-Methods", "OPTIONS,GET,POST,DELETE,PUT");
-        //cookie跨域的操作
-        response.setHeader("Access-Control-Allow-Credentials", "true");
+
         filterChain.doFilter(servletRequest,servletResponse);
     }
 
